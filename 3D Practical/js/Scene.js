1f8aa52624cfbe04c96c3b1efea91ebdb3e2e0d1
hbendekgey <henry.bendekgey@pomona.edu>
2017-10-18 13:01:39 +0200

    First attempt at building slowpoke (doesn't work)

--- 3D Practical/js/Scene.js
+++ 3D Practical/js/Scene.js
@@ -1,30 +1,23 @@
 "use strict";
 let Scene = function(gl) {
-  this.vsIdle = new Shader(gl, gl.VERTEX_SHADER, "idle_vs.essl");
-  this.fsSolid = new Shader(gl, gl.FRAGMENT_SHADER, "solid_fs.essl");
-  this.solidProgram = new Program(gl, this.vsIdle, this.fsSolid);
-
   this.vsTexture = new Shader(gl, gl.VERTEX_SHADER, "texture_vs.essl");
   this.fsTexture = new Shader(gl, gl.FRAGMENT_SHADER, "texture_fs.essl");
   this.textureProgram = new TexturedProgram(gl, this.vsTexture, this.fsTexture);
-  this.texture2D = new Texture2D(gl, "media/boom.png");
+  this.texture2DEye = new Texture2D(gl, "media/YadonEyeDh.png");
+  this.texture2DBody = new Texture2D(gl, "media/YadonDh.png");
 
-  this.staticMaterial = new Material(gl, this.solidProgram);
-  this.staticMaterial.time.set(0);
+  this.materials = [];
+  this.materials.push(new Material(gl, this.textureProgram));
+  this.materials.push(new Material(gl, this.textureProgram));
+  this.materials[0].colorTexture.set(this.texture2DBody);
+  this.materials[1].colorTexture.set(this.texture2DEye);
 
-  this.textureMaterial = new Material(gl, this.textureProgram);
-  this.textureMaterial.colorTexture.set(this.texture2D);
-  this.textureMaterial.boomPhase.set(new Vec2(0,0));
-  this.boomCounter = 0;
+  this.multiMesh = new MultiMesh(gl, "media/Slowpoke.json", this.materials);
+  this.gameObject = new GameObject(this.multiMesh);
+
   this.timeAtLastFrame = new Date().getTime();
-
-  this.triangleGeometry = new TriangleGeometry(gl);
-  this.textureGeometry = new TexturedQuadGeometry(gl);
-
   this.camera = new OrthoCamera();
   this.camera.updateViewProjMatrix();
-
-  this.gameObject = new GameObject(new Mesh(this.textureGeometry, this.textureMaterial));
 };
 
 Scene.prototype.update = function(gl, keysPressed) {
@@ -34,10 +27,6 @@ Scene.prototype.update = function(gl, keysPressed) {
   let timeAtThisFrame = new Date().getTime();
   let dt = (timeAtThisFrame - this.timeAtLastFrame) / 1000.0;
   this.timeAtLastFrame = timeAtThisFrame;
-
-  // animate boom
-  this.textureMaterial.boomPhase.set((Math.floor(this.boomCounter)%6), Math.floor(this.boomCounter/6)).mul(1/6);
-  this.boomCounter = (this.boomCounter + dt * 20) % 36
 
   // clear the screen
   gl.clearColor(0.2, 0, 0.2, 1.0);
