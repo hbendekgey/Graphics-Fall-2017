5563734ee8f9499eb8c9b25d74bb5b0c7cb8e4b5
hbendekgey <henry.bendekgey@pomona.edu>
2017-11-01 17:06:10 +0100

    Solid coloring, spinning rotor, and faster heli

--- 3D Homework/js/GameObject.js
+++ 3D Homework/js/GameObject.js
@@ -5,16 +5,24 @@ let GameObject = function(mesh) {
   this.position = new Vec3(0, 0, 0); 
   this.yaw = 0;
   this.pitch = 0;
-  this.roll = 0;
   this.right = new Vec3(0,0,1);
   this.scale = new Vec3(1,1,1); 
 
   this.modelMatrix = new Mat4(); 
 };
 
-GameObject.prototype.updateModelMatrix =
-                              function(){ 
-  this.modelMatrix.set().rotate(this.yaw,0,1,0).rotate(this.pitch,this.right).scale(this.scale).translate(this.position); 
+GameObject.prototype.updateModelMatrix = function() { 
+  this.modelMatrix.set().rotate(this.yaw,0,1,0)
+                        .rotate(this.pitch,this.right)
+                        .scale(this.scale)
+                        .translate(this.position);
+
+  if (this.parent) {
+    this.modelMatrix.rotate(this.parent.yaw,0,1,0)
+                    .rotate(this.parent.pitch,this.parent.right)
+                    .scale(this.parent.scale)
+                    .translate(this.parent.position);
+  }
 };
 
 GameObject.prototype.draw = function(camera, lightDirection){ 
--- 3D Homework/js/Scene.js
+++ 3D Homework/js/Scene.js
@@ -18,15 +18,42 @@ let Scene = function(gl) {
   this.groundMaterial = new Material(gl, this.textureProgram);
   this.groundMaterial.colorTexture.set(new Texture2D(gl, "media/sand.png"));
   this.textureGeometry = new TexturedQuadGeometry(gl);
-
-  this.gameObjects = [];
-  this.gameObjects.push(new GameObject(new Mesh(this.textureGeometry, this.groundMaterial)));
+  this.ground = new GameObject(new Mesh(this.textureGeometry, this.groundMaterial));
 
   this.camera = new PerspectiveCamera();
   this.camera.pitch = -0.1;
   this.camera.isDragging = true;
   this.camera.move(1, [], this.avatar);
   this.camera.isDragging = false;
+
+  this.gameObjects = [];
+
+  this.vsSolid = new Shader(gl, gl.VERTEX_SHADER, "solid_vs.essl");
+  this.fsSolid = new Shader(gl, gl.FRAGMENT_SHADER, "solid_fs.essl");
+  this.solidProgram = new TexturedProgram(gl, this.vsSolid, this.fsSolid);
+
+  this.rotorMaterials = [];
+  for (var i = 0; i < 2; i++) {
+    this.rotorMaterials.push(new Material(gl, this.solidProgram));
+    this.rotorMaterials[i].color.set(new Vec3(0.7,0.7,0.7));
+  }
+  this.rotorMultiMesh = new MultiMesh(gl, "media/heli/mainrotor.json", this.rotorMaterials);
+  this.gameObjects.push(new GameObject(this.rotorMultiMesh));
+  this.gameObjects[0].position.set(new Vec3(0,14,0));
+  this.gameObjects[0].parent = this.avatar;
+
+  this.treeTexture = new Texture2D(gl, "media/tree/tree.png");
+  this.treeMaterials = [];
+  this.treeMaterials.push(new Material(gl, this.textureProgram));
+  this.treeMaterials[0].colorTexture.set(this.treeTexture);
+  this.treeMultiMesh = new MultiMesh(gl, "media/tree/tree.json", this.treeMaterials);
+
+  for (var i = 1; i < 10; i++) {
+    this.gameObjects.push(new GameObject(this.treeMultiMesh));
+    this.gameObjects[i].position.addScaled(Math.random() - 0.5, new Vec3(1000,0,0));
+    this.gameObjects[i].position.addScaled(Math.random() - 0.5, new Vec3(0,0,1000));
+  }
+
 
   gl.enable(gl.DEPTH_TEST);
 
@@ -50,7 +77,9 @@ Scene.prototype.update = function(gl, keysPressed) {
 
   this.camera.move(dt, keysPressed, this.avatar);
   this.avatar.draw(this.camera, this.lightDirection);
-
+  this.ground.draw(this.camera, this.lightDirection);
+  this.gameObjects[0].yaw += 0.1;
+  
   for (var i = 0; i < this.gameObjects.length; i++) {
     this.gameObjects[i].draw(this.camera, this.lightDirection);
   }
