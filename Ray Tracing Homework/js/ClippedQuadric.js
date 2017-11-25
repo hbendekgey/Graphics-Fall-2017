78f7a6edd1b36b2de87001683a1d1ac17950ce93
hbendekgey <henry.bendekgey@pomona.edu>
2017-11-25 16:25:42 +0100

    Mirror Rook

--- Ray Tracing Homework/js/ClippedQuadric.js
+++ Ray Tracing Homework/js/ClippedQuadric.js
@@ -1,6 +1,7 @@
-let ClippedQuadric = function(surfaceCoeffMatrix, clipperCoeffMatrix) {
+let ClippedQuadric = function(surfaceCoeffMatrix, clipperCoeffMatrix, secondClipper) {
   this.surfaceCoeffMatrix = surfaceCoeffMatrix;
   this.clipperCoeffMatrix = clipperCoeffMatrix;
+  this.secondClipperCoeffMatrix = secondClipper
 }
 
 ClippedQuadric.prototype.setUnitSphere = function(){
@@ -52,10 +53,19 @@ ClippedQuadric.prototype.transform = function(transformationMatrix){
   let inverseTTranspose = new Mat4(inverseT).transpose();
   this.surfaceCoeffMatrix.premul(inverseT).mul(inverseTTranspose);
   this.clipperCoeffMatrix.premul(inverseT).mul(inverseTTranspose);
+  if (this.secondClipperCoeffMatrix) {
+  	 this.secondClipperCoeffMatrix.premul(inverseT).mul(inverseTTranspose);
+  }
 }
 
 ClippedQuadric.prototype.transformClipper = function(transformationMatrix){
   let inverseT = new Mat4(transformationMatrix).invert();
   let inverseTTranspose = new Mat4(inverseT).transpose();
   this.clipperCoeffMatrix.premul(inverseT).mul(inverseTTranspose);
+}
+
+ClippedQuadric.prototype.transformSecondClipper = function(transformationMatrix){
+  let inverseT = new Mat4(transformationMatrix).invert();
+  let inverseTTranspose = new Mat4(inverseT).transpose();
+  this.secondClipperCoeffMatrix.premul(inverseT).mul(inverseTTranspose);
 }
